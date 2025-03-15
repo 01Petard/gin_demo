f6f8843d066cef4656a884d1ecf972d142b5cc79
01Petard <1520394133@qq.com>
2025-03-15 19:52:26 +0800

    feat: 封装统一的请求和返回格式

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"gin_demo/common"
+	"gin_demo/dto"
 	"gin_demo/model"
+	"gin_demo/response"
 	"gin_demo/util"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -20,11 +22,21 @@ func Register(ctx *gin.Context) {
 
 	// 数据验证
 	if len(phone) != 11 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(
+			ctx,
+			http.StatusUnprocessableEntity,
+			422,
+			gin.H{}, "手机号必须为11位",
+		)
 		return
 	}
 	if len(password) < 6 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		response.Response(
+			ctx,
+			http.StatusUnprocessableEntity,
+			422,
+			gin.H{}, "密码不能少于6位",
+		)
 		return
 	}
 
@@ -32,7 +44,12 @@ func Register(ctx *gin.Context) {
 	var user model.User
 	DB.Where("phone = ?", phone).First(&user)
 	if user.ID != 0 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
+		response.Response(
+			ctx,
+			http.StatusUnprocessableEntity,
+			422,
+			gin.H{}, "用户已经存在",
+		)
 		return
 	}
 
@@ -40,7 +57,12 @@ func Register(ctx *gin.Context) {
 	// 加密密码
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误！"})
+		response.Response(
+			ctx,
+			http.StatusInternalServerError,
+			500,
+			gin.H{}, "加密错误！",
+		)
 	}
 
 	// 如果没有名称，则生成一个10为随机字符串
@@ -57,11 +79,11 @@ func Register(ctx *gin.Context) {
 
 	log.Println(name, phone, password)
 
-	ctx.JSON(http.StatusOK, gin.H{
+	response.Success(ctx, gin.H{
 		"code": "200",
 		"data": gin.H{},
 		"mag":  "登录成功！",
-	})
+	}, "注册成功！")
 }
 
 func Login(ctx *gin.Context) {
@@ -73,11 +95,21 @@ func Login(ctx *gin.Context) {
 
 	// 数据验证
 	if len(phone) != 11 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(
+			ctx,
+			http.StatusUnprocessableEntity,
+			422,
+			gin.H{}, "手机号必须为11位",
+		)
 		return
 	}
 	if len(password) < 6 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		response.Response(
+			ctx,
+			http.StatusUnprocessableEntity,
+			422,
+			gin.H{}, "密码不能少于6位",
+		)
 		return
 	}
 
@@ -85,35 +117,60 @@ func Login(ctx *gin.Context) {
 	var user model.User
 	DB.Where("phone = ?", phone).First(&user)
 	if user.ID == 0 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在，请先注册！"})
+		response.Response(
+			ctx,
+			http.StatusUnprocessableEntity,
+			422,
+			gin.H{}, "用户不存在，请先注册！",
+		)
 		return
 	}
 
 	// 判断密码是否正确
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误！"})
+		response.Response(
+			ctx,
+			http.StatusBadRequest,
+			400,
+			gin.H{}, "密码错误！")
 	}
 
 	// 发放token
 	token, err := common.ReleaseToken(user)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统错误！"})
+		response.Response(
+			ctx,
+			http.StatusInternalServerError,
+			500,
+			gin.H{}, "系统错误！",
+		)
 		log.Printf("token generate error: %v", err)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": "200",
-		"data": gin.H{"token": token},
-		"mag":  "登录成功！",
-	})
+	response.Success(
+		ctx,
+		gin.H{
+			"code": "200",
+			"data": gin.H{
+				"token": token,
+			},
+			"mag": "登录成功！",
+		},
+		"登录成功！")
 }
 
 func Info(ctx *gin.Context) {
 	user, _ := ctx.Get("user")
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": "200",
-		"data": gin.H{"user": user}})
+	response.Success(
+		ctx,
+		gin.H{
+			"code": "200",
+			"data": gin.H{
+				"user": dto.ToUserDto(user.(model.User))},
+		},
+		"获取用户信息成功！",
+	)
 
 }
---
+++ dto/userdto.go
@@ -0,0 +1,17 @@
+package dto
+
+import (
+	"gin_demo/model"
+)
+
+type UserDto struct {
+	Name  string `json:"name"`
+	Phone string `json:"phone"`
+}
+
+func ToUserDto(user model.User) UserDto {
+	return UserDto{
+		Name:  user.Name,
+		Phone: user.Phone,
+	}
+}
