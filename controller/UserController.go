46015f98eee9922bc69d5c9537b844f3101337d1
01Petard <1520394133@qq.com>
2025-03-15 18:58:21 +0800

    feat: 用户登录

--- controller/UserController.go
+++ controller/UserController.go
@@ -5,7 +5,7 @@ import (
 	"gin_demo/model"
 	"gin_demo/util"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -13,27 +13,36 @@ import (
 func Register(ctx *gin.Context) {
 	DB := common.GetDB()
 
+	// 获取参数
 	name := ctx.PostForm("name")
 	phone := ctx.PostForm("phone")
 	password := ctx.PostForm("password")
 
+	// 数据验证
 	if len(phone) != 11 {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
 		return
 	}
-
 	if len(password) < 6 {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
 		return
 	}
 
 	// 判断手机号是否存在，用户是否存在
-	if isPhoneExist(DB, phone) {
+	var user model.User
+	DB.Where("phone = ?", phone).First(&user)
+	if user.ID != 0 {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
 		return
 	}
 
 	// 创建新用户
+	// 加密密码
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误！"})
+	}
+
 	// 如果没有名称，则生成一个10为随机字符串
 	if len(name) == 0 {
 		name = util.RandomString(10)
@@ -41,7 +50,7 @@ func Register(ctx *gin.Context) {
 	newUser := model.User{
 		Name:     name,
 		Phone:    phone,
-		Password: password,
+		Password: string(hashedPassword),
 	}
 
 	DB.Create(&newUser)
@@ -49,15 +58,48 @@ func Register(ctx *gin.Context) {
 	log.Println(name, phone, password)
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"mag": "注册成功！",
+		"code": "200",
+		"data": gin.H{},
+		"mag":  "登录成功！",
 	})
 }
 
-func isPhoneExist(db *gorm.DB, phone string) bool {
-	var user model.User
-	db.Where("phone = ?", phone).First(&user)
-	if user.ID != 0 {
-		return true
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+
+	// 获取参数
+	phone := ctx.PostForm("phone")
+	password := ctx.PostForm("password")
+
+	// 数据验证
+	if len(phone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		return
 	}
-	return false
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		return
+	}
+
+	// 判断手机号是否存在，用户是否存在
+	var user model.User
+	DB.Where("phone = ?", phone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在，请先注册！"})
+		return
+	}
+
+	// 判断密码是否正确
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误！"})
+	}
+
+	// 发放token
+	token := "111"
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": "200",
+		"data": gin.H{"token": token},
+		"mag":  "登录成功！",
+	})
 }
